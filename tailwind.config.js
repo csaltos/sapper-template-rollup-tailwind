7853586e34f817e453f191792c1d807a06a51d7e
Carlos Saltos <csaltos@gmail.com>
2020-09-23 22:23:00 +0300

    Adding colors via buildPalette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,37 @@
+//const { addUtility } = require("smelte/src/utils/style.js");
+
+const buildPalette = require("smelte/src/utils/color.js");
+
+const colors = {
+  primary: "#b027b0",
+  secondary: "#009688",
+  error: "#f44336",
+  success: "#4caf50",
+  alert: "#ff9800",
+  blue: "#2196f3",
+  dark: "#212121",
+
+  // These are material palette colors. You should keep only colors that you're using.
+  // red: "#f44336",
+  // pink: "#e91e63",
+  // purple: "#9c27b0",
+  // "deep-purple": "#673ab7",
+  // indigo: "#3f51b5",
+  // blue: "#2196f3",
+  // "light-blue": "#03a9f4",
+  // cyan: "#00bcd4",
+  // teal: "#009688",
+  // green: "#4caf50",
+  // "light-green": "#8bc34a",
+  // lime: "#cddc39",
+  // yellow: "#ffeb3b",
+  // amber: "#ffc107",
+  orange: "#ff9800",
+  // "deep-orange": "#ff5722",
+  // brown: "#795548"
+};
+
+
 module.exports = {
   purge: [
     './src/**/*.svelte',
@@ -5,81 +39,7 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      dark: {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121",
-        trans: "rgba(250, 250, 250, 0.5)",
-        transLight: "rgba(250, 250, 250, 0.1)",
-        transDark: "rgba(100, 100, 100, 0.2)"
-      },
-      blue: {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121",
-        trans: "rgba(250, 250, 250, 0.5)",
-        transLight: "rgba(250, 250, 250, 0.1)",
-        transDark: "rgba(100, 100, 100, 0.2)"
-      },
-      orange: {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121",
-        trans: "rgba(250, 250, 250, 0.5)",
-        transLight: "rgba(250, 250, 250, 0.1)",
-        transDark: "rgba(100, 100, 100, 0.2)"
-      },
-      primary: {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121",
-        trans: "rgba(250, 250, 250, 0.5)",
-        transLight: "rgba(250, 250, 250, 0.1)",
-        transDark: "rgba(100, 100, 100, 0.2)"
-      },
-      secondary: {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121",
-        trans: "rgba(250, 250, 250, 0.5)",
-        transLight: "rgba(250, 250, 250, 0.1)",
-        transDark: "rgba(100, 100, 100, 0.2)"
-      },
+      ...buildPalette(colors),
       gray: {
         "50": "#fafafa",
         "100": "#f5f5f5",
@@ -94,97 +54,7 @@ module.exports = {
         trans: "rgba(250, 250, 250, 0.5)",
         transLight: "rgba(250, 250, 250, 0.1)",
         transDark: "rgba(100, 100, 100, 0.2)"
-      },
-      dark3: {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121",
-        trans: "rgba(250, 250, 250, 0.5)",
-        transLight: "rgba(250, 250, 250, 0.1)",
-        transDark: "rgba(100, 100, 100, 0.2)"
-      },
-      dark4: {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121",
-        trans: "rgba(250, 250, 250, 0.5)",
-        transLight: "rgba(250, 250, 250, 0.1)",
-        transDark: "rgba(100, 100, 100, 0.2)"
-      },
-      dark5: {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121",
-        trans: "rgba(250, 250, 250, 0.5)",
-        transLight: "rgba(250, 250, 250, 0.1)",
-        transDark: "rgba(100, 100, 100, 0.2)"
-      },
-      dark6: {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121",
-        trans: "rgba(250, 250, 250, 0.5)",
-        transLight: "rgba(250, 250, 250, 0.1)",
-        transDark: "rgba(100, 100, 100, 0.2)"
-      },
-      dark7: {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121",
-        trans: "rgba(250, 250, 250, 0.5)",
-        transLight: "rgba(250, 250, 250, 0.1)",
-        transDark: "rgba(100, 100, 100, 0.2)"
-      },
-      dark8: {
-        "50": "#fafafa",
-        "100": "#f5f5f5",
-        "200": "#eeeeee",
-        "300": "#e0e0e0",
-        "400": "#bdbdbd",
-        "500": "#9e9e9e",
-        "600": "#757575",
-        "700": "#616161",
-        "800": "#424242",
-        "900": "#212121",
-        trans: "rgba(250, 250, 250, 0.5)",
-        transLight: "rgba(250, 250, 250, 0.1)",
-        transDark: "rgba(100, 100, 100, 0.2)"
-      },
+      }
     }
   },
   variants: {},
